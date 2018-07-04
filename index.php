62f8bc2778e7f7f9c59d9e2c3058c5a4d24e90e4
adrianodefendi <adrianodefendi@gmail.com>
2018-07-04 09:19:06 -0300

    add index

---
+++ index.php
@@ -0,0 +1,10 @@
+﻿<?php
+$title = "Franquia Purific - franchising Purific - Seja franqueado";
+$description = "Conheça a Franquia Purific e seja um franqueado.";
+
+include_once "header.php";
+
+include_once "home.php";
+
+include_once "footer.php";
+?>
